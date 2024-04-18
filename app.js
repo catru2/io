42c5f857a433d202a8e2b7203334eedc8cf6c5b9
catru2 <camachotrujillomartinx@gmail.com>
2024-04-17 23:20:08 -0600

    servidor io

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require("cors");
+const {Server: SocketServer} = require("socket.io");
+const http =require("http");
+
+
+const app = express();
+const server = http.createServer(app);  
+const io = new SocketServer(server, {
+    cors: {
+       origin: "*" 
+},
+})
+
+app.use(cors());
+
+io.on('connection', (socket) =>{
+    console.log("Cliente conectado")
+
+
+io.close('disconnect', () =>{
+    console.log('Cliente desconectado')
+});
+
+socket.on('esp32', (msg)=>{
+      console.log(msg)
+      io.emit("esp32", msg)
+})
+
+
+});
+
+const PORT = process.env.PORT || 3000;
