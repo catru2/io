6502ebc85b99ea5f666be7fc35c1d5d0fbff3458
catru2 <camachotrujillomartinx@gmail.com>
2024-04-18 01:03:28 -0600

    cambios echos

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const express = require('express');
 const cors = require("cors");
-const {Server: SocketServer} = require("socket.io");
-const http =require("http");
-
+const { Server: SocketServer } = require("socket.io");
+const http = require("http");
 
 const app = express();
-const server = http.createServer(app);  
+const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
-       origin: "*" 
-},
-})
+        origin: "*"
+    }
+});
 
 app.use(cors());
 
-io.on('connection', (socket) =>{
-    console.log("Cliente conectado")
+io.on('connection', (socket) => {
+    console.log("Cliente conectado");
 
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
 
-io.close('disconnect', () =>{
-    console.log('Cliente desconectado')
-});
-
-socket.on('esp32', (msg)=>{
-      console.log(msg)
-      io.emit("esp32", msg)
-})
-
-
+    socket.on('esp32', (msg) => {
+        console.log(msg);
+        io.emit("esp32", msg);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
+
+
+
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
